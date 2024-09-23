0975e5a15041f956863ff3ee3b83f47d8c467feb
caringallawrence <caringallawrence5@gmail.com>
2024-09-23 18:40:28 +1200

    inits commit

---
+++ resources/views/contents/about.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/about.blade.php -->
+@extends('layouts.main')
+
+@section('title', 'About Page')
+
+@section('content')
+    <h2>About Us</h2>
+    <p>This is the About page content, using the main layout.</p>
+@endsection
---
+++ resources/views/contents/home.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/home.blade.php -->
+@extends('layouts.main')
+
+@section('title', 'Home Page')
+
+@section('content')
+    <h2>Hello, World!</h2>
+    <p>This is a simple Home page using the main layout.</p>
+@endsection
